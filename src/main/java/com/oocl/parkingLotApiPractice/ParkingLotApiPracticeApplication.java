6e9a1df0588a06a7993ef8ae36d0aea3cda5ec06
SiuKeung <siu_keung@outlook.com>
2018-07-25 13:56:39 +0800

    创建了工程

---
+++ src/main/java/com/oocl/parkingLotApiPractice/ParkingLotApiPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.parkingLotApiPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotApiPracticeApplication.class, args);
+	}
+}
